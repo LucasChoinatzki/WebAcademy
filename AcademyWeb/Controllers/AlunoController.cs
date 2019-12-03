07577dd09ca233b97afbce306cdf723293f01a1a
LucasChoinatzki <55769995+LucasChoinatzki@users.noreply.github.com>
2019-12-03 20:14:11 -0300

    Academia

---
+++ AcademyWeb/Controllers/AlunoController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Repository;
+
+namespace WebAcademy.Controllers
+{
+    public class AlunoController : Controller
+    {
+        private readonly ExercicioDAO _exercicioDAO;
+        private readonly TreinoDAO _treinoDAO;
+            private readonly AlunoDAO _alunoDAO;
+            private readonly AulaDAO _aulaDAO;
+            private readonly IHostingEnvironment _hosting;
+
+            public AlunoController(AlunoDAO alunoDAO, IHostingEnvironment hosting, AulaDAO aulaDAO, ExercicioDAO exercicioDAO,TreinoDAO treinoDAO)
+            {
+                _alunoDAO = alunoDAO;
+                _hosting = hosting;
+                _aulaDAO = aulaDAO;
+                _exercicioDAO = exercicioDAO;
+                _treinoDAO = treinoDAO;
+            }
+
+            [HttpPost]
+            public IActionResult Cadastrar(Aluno a, IFormFile fupImagem)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+                //Cadastrar a imagem
+                if (fupImagem != null)
+                {
+                    //paste: ecommerceimagens
+                    string arquivo = Guid.NewGuid().ToString() +
+                        Path.GetExtension(fupImagem.FileName);
+                    string caminho = Path.Combine(_hosting.WebRootPath,
+                        "Academyimagens", arquivo);
+                    fupImagem.CopyTo(
+                        new FileStream(caminho, FileMode.Create));
+                    a.Imagem = arquivo;
+                }
+                else
+                {
+                    a.Imagem = "semimagem.jfif";
+                }
+
+                if (_alunoDAO.Cadastrar(a))
+                {
+                    return RedirectToAction("GerenciamentoAluno");
+                }
+                ModelState.AddModelError
+                    ("", "Esse Aluno já existe!");
+            }
+            return View(a);
+        }
+        public IActionResult IndexAluno()
+            {
+                ViewBag.DataHora = DateTime.Now;
+                return View(_alunoDAO.ListarTodos());
+            }
+
+        
+        public IActionResult Cadastrar()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Alterarb(Aluno a)
+        {
+            _alunoDAO.Alterar(a);
+            return RedirectToAction("EditarPerfil",a);
+        }
+        public IActionResult GerenciamentoAluno()
+        {
+            ViewBag.DataHora = DateTime.Now;
+            return View(_alunoDAO.ListarTodos());
+
+        }
+        public IActionResult Alterar(int id)
+        {
+            Aluno b = _alunoDAO.BuscarPorId(id);
+            return View(b);
+        }
+        public IActionResult ViewAula()
+        {
+            ViewBag.DataHora = DateTime.Now;
+            return View(_aulaDAO.ListarTodos());
+
+        }
+        public IActionResult AlterarAula(int id)
+        {
+            Aula a = _aulaDAO.BuscarPorId(id);
+            int v = a.Vagas;
+
+            if (v > 0)
+            {
+                a.Vagas = v - 1;
+                _aulaDAO.AlterarAula(a);
+                return RedirectToAction("ViewAula");
+            }
+
+            return RedirectToAction("ViewAula");
+        }
+        public IActionResult ViewTreino()
+
+        { 
+            
+            return View();
+        }
+        
+        public IActionResult ValidarAluno(Aluno a)
+
+        {
+            
+            Aluno b = _alunoDAO.ValidarSenha(a);
+            if(b != null)
+            {
+                return RedirectToAction("EditarPerfil",b);
+                
+            }
+            else {
+                
+                return RedirectToAction("EditarPerfil",1);
+            }
+        }
+        public IActionResult EditarPerfil(Aluno a, int v)
+
+        {
+            if (v == 1) { 
+            ModelState.AddModelError
+                    ("", "Login Invalido");
+                return View();
+            }
+            return View(a);
+        }
+
+        
+
+    }
+    
+}
---
+++ Repository/ExercicioDAO.cs
@@ -0,0 +1,43 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ExercicioDAO : IRepository<Exercicio>
+    {
+
+        private readonly Context ctx;
+        public ExercicioDAO(Context context)
+        {
+            ctx = context;
+        }
+
+
+        public bool Cadastrar(Exercicio ex)
+        {
+            ctx.Exercicios.Add(ex);
+            ctx.SaveChanges();
+            return true;
+        }
+        public List<Exercicio> ListarTodos()
+        {
+            return ctx.Exercicios.ToList();
+        }
+
+
+        public List<Exercicio> ListarporLista(ListaTreino listaTreino)
+        {
+            return ctx.Exercicios.Include(x => x.ListaTreino).Include(x => x.Equipamento).Include(x => x.Quantidade).Include(x => x.Repeticoes).Where(x => x.ListaTreino == listaTreino).ToList();
+            
+        }
+        public Exercicio BuscarPorId(int id)
+        {
+            return ctx.Exercicios.Find(id);
+        }
+
+    }
+}
